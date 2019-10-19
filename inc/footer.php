5190447468b354f206f58a5474fa78290ba50588
c0rv07 <crow7@protonmail.com>
2019-10-19 15:32:53 +0200

    fix path

--- inc/footer.php
+++ inc/footer.php
@@ -11,16 +11,16 @@ if(!defined('__CONFIG__')) {
 
 
 <!-- jQuery is requierd -->
-    <script src="assets/js/jquery-3.4.1.min.js" Type="text/javascript"></script>
+    <script src="PHP-Login-System/assets/js/jquery-3.4.1.min.js" Type="text/javascript"></script>
 
      <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js" Type="text/javascript" ></script> 
 
 <!-- UIkit JS -->
 
-<script src="assets/js/uikit.min.js"Type="text/javascript" ></script>
+<script src="PHP-Login-System/assets/js/uikit.min.js"Type="text/javascript" ></script>
 
 
-<script src="assets/js/uikit.icons.min.js" Type="text/javascript"></script>
+<script src="PHP-Login-System/assets/js/uikit.icons.min.js" Type="text/javascript"></script>
 
 <script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.2.1/js/uikit.min.js" Type="text/javascript"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.2.1/js/uikit-icons.min.js" Type="text/javascript"></script>
@@ -29,4 +29,4 @@ if(!defined('__CONFIG__')) {
 
 
 
-<script src="/assets/js/main.js" Type="text/javascript"></script>
+<script src="PHP-Login-System/assets/js/main.js" Type="text/javascript"></script>
